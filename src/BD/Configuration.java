7e54e494132ca541b6b74be2da93e6de4b9cfa7a
jinranxi100 <73112223+jinranxi100@users.noreply.github.com>
2020-11-02 19:40:58 +0100

    2

--- src/BD/Configuration.java
+++ src/BD/Configuration.java
@@ -1,5 +1,78 @@
 package BD;
 
 public class Configuration {
+	private String driver;
+    /**
+     * jdbc url
+     */
+    private String url;
 
+    /**
+     * user
+     */
+    private String user;
+
+    /**
+     * mdp
+     */
+    private String pwd;
+
+    /**
+     * db name
+     */
+    private String usingDB;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getPwd() {
+        return pwd;
+    }
+
+    public void setPwd(String pwd) {
+        this.pwd = pwd;
+    }
+
+    public String getUsingDB() {
+        return usingDB;
+    }
+
+    public void setUsingDB(String usingDB) {
+        this.usingDB = usingDB;
+    }
+
+    public  String getDriver() {
+        return driver;
+    }
+
+    public  void setDriver(String driver) {
+       this.driver = driver;
+    }
+
+    public Configuration(String driver,String url, String user, String pwd, String usingDB) {
+        this.driver = driver;
+        this.url = url;
+        this.user = user;
+        this.pwd = pwd;
+        this.usingDB = usingDB;
+    }
+
+    
+
+    public Configuration() {
+        super();
+    }
 }
--- src/BD/DB.java
+++ src/BD/DB.java
@@ -1,5 +1,98 @@
 package BD;
 
-public class BD {
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Properties;
+
+public class DB {
+	private static Configuration conf;
+
+	/**
+	 *
+	 */
+	static {
+		String path = "BD//db.properties";
+		Properties pros = new Properties();
+		try {
+			pros.load(Thread.currentThread().getContextClassLoader().getResourceAsStream(path));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		conf = new Configuration();
+		conf.setPwd(pros.getProperty("pwd"));
+		conf.setUrl(pros.getProperty("url"));
+		conf.setUser(pros.getProperty("user"));
+		conf.setUsingDB(pros.getProperty("usingDB"));
+		conf.setDriver(pros.getProperty("driver"));
+
+	}
+
+	/**
+	 * connexion¡§
+	 *
+	 * @return
+	 */
+	// R¨¦aliser la connexion avec la base de donn¨¦es
+	public static Connection createConn() {
+		try {
+			Class.forName(conf.getDriver());
+			return DriverManager.getConnection(conf.getUrl() + conf.getUsingDB(), conf.getUser(), conf.getPwd());
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	/**
+	 * @return Configuration
+	 */
+	public static Configuration getConf() {
+		return conf;
+	}
+
+// Pour ¨¦conomiser des ressources, fermez la base de donn¨¦es    
+	public static void close(Connection con) {
+		if (con != null) {
+			try {
+				con.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+				throw new RuntimeException("failed", e);
+			}
+		}
+	}
+
+	/**
+	 * @param rs
+	 * @param ps
+	 * @param conn
+	 */
+	public static void close(ResultSet rs, Statement ps, Connection conn) {
+		try {
+			if (rs != null) {
+				rs.close();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		try {
+			if (ps != null) {
+				ps.close();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		try {
+			if (conn != null) {
+				conn.close();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
 
 }
