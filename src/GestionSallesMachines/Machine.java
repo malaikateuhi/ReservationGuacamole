ea4c3dc9b5d88ffc6645c673ec81d1313c02c716
meng7777 <66383612+meng7777@users.noreply.github.com>
2020-11-03 15:25:19 +0100

    Update Machine.java

--- src/GestionSallesMachines/Machine.java
+++ src/GestionSallesMachines/Machine.java
@@ -1,9 +1,14 @@
 package GestionSallesMachines;
 
+import java.util.HashMap;
+
+import GestionReservations.Reservation;
+import GestionUtilisateurs.Etudiant;
+
 public class Machine {
 	private String numMachine;
 	private EtatMachineSalle etat;
 	
-	
+	private HashMap<Etudiant,Reservation> hmReser;
 	
 }
