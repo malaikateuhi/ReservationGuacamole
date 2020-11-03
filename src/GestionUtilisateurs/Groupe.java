094c70df19e02bebf4658c49636d2f69457d9650
meng7777 <66383612+meng7777@users.noreply.github.com>
2020-11-03 10:46:04 +0100

    Update Groupe.java

--- src/GestionUtilisateurs/Groupe.java
+++ src/GestionUtilisateurs/Groupe.java
@@ -1,6 +1,10 @@
 package GestionUtilisateurs;
 
+import java.util.ArrayList;
+
 public class Groupe {
 	private String numGroupe;
 	private String nomGroupe;
+	
+	private ArrayList<Etudiant> lstEtu;
 }
