477947618b57ee1b8ee936f74cb313dd000fd6c7
meng7777 <66383612+meng7777@users.noreply.github.com>
2020-11-03 16:48:57 +0100

    Update Admin.java

--- src/GestionUtilisateurs/Admin.java
+++ src/GestionUtilisateurs/Admin.java
@@ -10,25 +10,25 @@ public class Admin extends Utilisateur{
 		return super.getIdentifiant();
 	}
 	public void setIdentifiant(String identifiant) {
-		super.setIdentifiant(identifiant);;
+		super.setIdentifiant(identifiant);
 	}
 	public String getMdp() {
 		return super.getMdp();
 	}
 	public void setMdp(String mdp) {
-		super.setMdp(mdp);;
+		super.setMdp(mdp);
 	}
 	public String getNom() {
 		return super.getNom();
 	}
 	public void setNom(String nom) {
-		super.setNom(nom);;
+		super.setNom(nom);
 	}
 	public String getPrenom() {
 		return super.getPrenom();
 	}
 	public void setPrenom(String prenom) {
-		super.setPrenom(prenom);;
+		super.setPrenom(prenom);
 	}
 	public void ajouterMachine() {
 		
