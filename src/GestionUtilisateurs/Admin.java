cb46f0938607ba9f8a8fa1611d7112ab70cc7472
jinranxi100 <73112223+jinranxi100@users.noreply.github.com>
2020-11-03 15:54:44 +0100

    merge dev

--- src/GestionUtilisateurs/Admin.java
+++ src/GestionUtilisateurs/Admin.java
@@ -3,7 +3,7 @@ package GestionUtilisateurs;
 public class Admin extends Utilisateur{
 
 	public Admin(String identifiant, String mdp, String nom, String prenom) {
-		super(identifiant, mdp, nom, prenom);
+		super();
 	}
 	
 	public void ajouterMachine() {
