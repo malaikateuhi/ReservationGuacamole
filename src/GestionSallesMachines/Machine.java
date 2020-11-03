7a33ebe1381f19a2598079dabdbf24cc6d733329
meng7777 <66383612+meng7777@users.noreply.github.com>
2020-11-03 17:08:00 +0100

    Update Machine.java

--- src/GestionSallesMachines/Machine.java
+++ src/GestionSallesMachines/Machine.java
@@ -11,4 +11,33 @@ public class Machine {
 	
 	private HashMap<Etudiant,Reservation> hmReser;
 	
+	public Machine() {
+	
+	}
+	public Machine(String numMachine, EtatMachineSalle etat, HashMap<Etudiant, Reservation> hmReser) {
+		super();
+		this.numMachine = numMachine;
+		this.etat = etat;
+		this.hmReser = hmReser;
+	}
+	public String getNumMachine() {
+		return numMachine;
+	}
+	public void setNumMachine(String numMachine) {
+		this.numMachine = numMachine;
+	}
+	public EtatMachineSalle getEtat() {
+		return etat;
+	}
+	public void setEtat(EtatMachineSalle etat) {
+		this.etat = etat;
+	}
+	public HashMap<Etudiant, Reservation> getHmReser() {
+		return hmReser;
+	}
+	public void setHmReser(HashMap<Etudiant, Reservation> hmReser) {
+		this.hmReser = hmReser;
+	}
+	
+	
 }
