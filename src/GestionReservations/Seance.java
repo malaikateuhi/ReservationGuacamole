2860e1a1286c052120fb0b07571d3141e663680b
meng7777 <66383612+meng7777@users.noreply.github.com>
2020-11-03 10:45:53 +0100

    Update Seance.java

--- src/GestionReservations/Seance.java
+++ src/GestionReservations/Seance.java
@@ -1,8 +1,13 @@
 package GestionReservations;
 
+import GestionUtilisateurs.Enseignant;
+import GestionUtilisateurs.Groupe;
+
 public class Seance {
 	private String numSeance;
 	private String nomCours;
 	private String etat;
 	
+	private Groupe groupeEtu;
+	private Enseignant ensignant;
 }
