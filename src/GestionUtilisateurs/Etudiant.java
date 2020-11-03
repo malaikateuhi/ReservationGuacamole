0c49d9e0353e3415b7bf2960386095083a4a85b1
meng7777 <66383612+meng7777@users.noreply.github.com>
2020-11-03 16:45:57 +0100

    Update Etudiant.java

--- src/GestionUtilisateurs/Etudiant.java
+++ src/GestionUtilisateurs/Etudiant.java
@@ -1,6 +1,35 @@
 package GestionUtilisateurs;
 
 public class Etudiant extends Utilisateur {
+	public Etudiant() {
+		
+	}
+	
+	public String getIdentifiant() {
+		return super.getIdentifiant();
+	}
+	public void setIdentifiant(String identifiant) {
+		super.setIdentifiant(identifiant);;
+	}
+	public String getMdp() {
+		return super.getMdp();
+	}
+	public void setMdp(String mdp) {
+		super.setMdp(mdp);;
+	}
+	public String getNom() {
+		return super.getNom();
+	}
+	public void setNom(String nom) {
+		super.setNom(nom);;
+	}
+	public String getPrenom() {
+		return super.getPrenom();
+	}
+	public void setPrenom(String prenom) {
+		super.setPrenom(prenom);;
+	}
+	
 	public void reserver() {
 		
 	}
