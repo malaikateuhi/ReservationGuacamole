617cef84f5de75f79aea83b6cb3d71045e51a917
jinranxi100 <73112223+jinranxi100@users.noreply.github.com>
2020-11-04 00:12:05 +0100

    sql coeur de reserver

--- src/Dao/AdminDao.java
+++ src/Dao/AdminDao.java
@@ -4,10 +4,15 @@ import static BD.Query.Query;
 import static BD.Query.Select;
 import static BD.Query.afferentSQL;
 import static BD.Query.parameter;
+
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+import GestionReservations.Reservation;
+import GestionSallesMachines.Machine;
 import GestionUtilisateurs.Admin;
+import GestionUtilisateurs.Etudiant;
 
 
 public class AdminDao {
@@ -17,17 +22,14 @@ public class AdminDao {
 	    String sqladmin = "select * from admin where ida=? and passworda=? ";//faut coorriger le password dans bd admin
 	    Admin ares = null;
 	    Query();
-	    
 	    parameter.add(id);
 	    parameter.add(password);
 	    afferentSQL(sqladmin);
-	    
-	    
 	    List<Object> objs = Select();
 	    System.out.println(objs.get(0));
 	    Map<String, Object> rowData =(Map<String, Object>)objs.get(0);
-	  
-	    if (objs.size()!=0) {
+
+           if (objs.size()!=0) {
 	    	/*
 	    	  ares = new Admin();
 	          ares.setNuma((String) rowData.get("ida"));
@@ -42,6 +44,48 @@ public class AdminDao {
 	    else {
 	    return null;}
 	    }
+	
+	ArrayList<Machine> lstMachine;
+	ArrayList<Etudiant> lstEtudiant;
+	public ArrayList<Machine> tousmachine ()
+	{
+		String sqlreserver = "select * from machine";
+		Query(); 
+		afferentSQL(sqlreserver);
+		this.lstMachine= new ArrayList<Machine>();
+		  List<Object> objs = Select();
+	        for (int i = 0; i < objs.size(); i++) {
+	        	Map<String, Object> rowData = (Map<String, Object>) objs.get(i);
+	        	Machine machine= new Machine();
+	        	machine.setNumMachine((String)rowData.get("jour"));
+	        	machine.setEtat((String)rowData.get("etatr"));
+	            lstMachine.add(machine);
+	        }
+		return lstMachine;
+	}
+
+	
+	
+	public ArrayList<Etudiant> tousetudiant (){
+		String sqlreserver = "select * from etudiant";
+		Query(); 
+		afferentSQL(sqlreserver);
+		this.lstEtudiant= new ArrayList<Etudiant>();
+		  List<Object> objs = Select();
+	        for (int i = 0; i < objs.size(); i++) {
+	        	Map<String, Object> rowData = (Map<String, Object>) objs.get(i);
+	        	Etudiant etudiant= new Etudiant();
+	        	etudiant.setIdentifiant((String)rowData.get("ide"));
+	        	etudiant.setMdp((String)rowData.get("passworde"));
+	        	etudiant.setNom((String)rowData.get("nome"));
+	        	etudiant.setPrenom((String)rowData.get("prenome"));
+	            lstEtudiant.add(etudiant);
+	        }
+		return lstEtudiant;
+	}
+
+	
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		AdminDao a1 = new AdminDao();	
--- src/GestionSallesMachines/Machine.java
+++ src/GestionSallesMachines/Machine.java
@@ -3,6 +3,25 @@ package GestionSallesMachines;
 public class Machine {
 	public String numMachine;
 	public String etat;
+	public String numsalle;
+	
+	
+	
+	public Machine() {
+		
+	}
+	public String getNumsalle() {
+		return numsalle;
+	}
+	public void setNumsalle(String numsalle) {
+		this.numsalle = numsalle;
+	}
+	public String getNumMachine() {
+		return numMachine;
+	}
+	public void setNumMachine(String numMachine) {
+		this.numMachine = numMachine;
+	}
 	public String getEtat() {
 		return etat;
 	}
