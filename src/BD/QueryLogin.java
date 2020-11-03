0675d6a41762c75d4f3e3df16d385323ed35236d
meng7777 <66383612+meng7777@users.noreply.github.com>
2020-11-03 20:58:43 +0100

    Update QueryLogin.java

--- src/BD/QueryLogin.java
+++ src/BD/QueryLogin.java
@@ -12,19 +12,44 @@ public class QueryLogin {
 	public QueryLogin() {
 		
 	}
-	// cette methode permet de trouver les infos de client en fonction de son email et mot de passe
-	public Utilisateur login(Connection con,Utilisateur user)throws Exception{
-		Utilisateur resUser=null;
-		String sql = null;
-		if(user instanceof Etudiant) {
-			sql="select * from etudiant where ide=? and mdpe=?";
+	public Etudiant loginEtu(Connection con,Utilisateur user)throws Exception{
+		Etudiant resUser = null;
+		String sql = "select * from etudiant where ide=? and passworde=?";
+
+		PreparedStatement pstmt = con.prepareStatement(sql);
+
+		pstmt.setString(1, user.getIdentifiant());
+		pstmt.setString(2, user.getMdp());
+		ResultSet rs=pstmt.executeQuery();
+		if (rs.next()) {
+		
+				resUser=new Etudiant();
+				resUser.setIdentifiant(rs.getString("ide"));
+				resUser.setMdp(rs.getString("passworde"));
+			
 		}
-		else if(user instanceof Enseignant) {
-			sql="select * from enseignant where iden=? and mdpen=?";
+		return resUser;
+	}
+	public Enseignant loginEn(Connection con,Utilisateur user)throws Exception{
+		Enseignant resUser = null;
+		String sql = "select * from enseignant where iden=? and passworden=?";
+
+		PreparedStatement pstmt = con.prepareStatement(sql);
+		pstmt.setString(1, user.getIdentifiant());
+		pstmt.setString(2, user.getMdp());
+		ResultSet rs=pstmt.executeQuery();
+		if (rs.next()) {
+		
+				resUser=new Enseignant();
+				resUser.setIdentifiant(rs.getString("iden"));
+				resUser.setMdp(rs.getString("passworden"));
+			
 		}
-		else {
-			sql="select * from admin where ida=? and mdpa=?";
-		}
+		return resUser;
+	}
+	public Admin loginAdmin(Connection con,Utilisateur user)throws Exception{
+		Admin resUser=null;
+		String sql = "select * from admin where ida=? and passworda=?";
 		
 		PreparedStatement pstmt = con.prepareStatement(sql);
 
@@ -32,22 +57,13 @@ public class QueryLogin {
 		pstmt.setString(2, user.getMdp());
 		ResultSet rs=pstmt.executeQuery();
 		if (rs.next()) {
-			if(user instanceof Etudiant) {
-				resUser=new Etudiant();
-				resUser.setIdentifiant(rs.getString("ide"));
-				resUser.setMdp(rs.getString("mdpe"));
-			}
-			else if(user instanceof Enseignant) {
-				resUser=new Enseignant();
-				resUser.setIdentifiant(rs.getString("iden"));
-				resUser.setMdp(rs.getString("mdpen"));
-			}
-			else {
+			
 				resUser=new Admin();
 				resUser.setIdentifiant(rs.getString("ida"));
-				resUser.setMdp(rs.getString("mdpa"));
-			}
+				resUser.setMdp(rs.getString("passworda"));
 		}
+		
 		return resUser;
 	}
+
 }
