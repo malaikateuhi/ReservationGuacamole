5a8a289fa05a81620ab399e240c1a8e980c51897
meng7777 <66383612+meng7777@users.noreply.github.com>
2020-11-03 15:25:17 +0100

    Update TempsDeSeance.java

--- src/GestionReservations/TempsDeSeance.java
+++ src/GestionReservations/TempsDeSeance.java
@@ -1,8 +1,13 @@
 package GestionReservations;
 import java.util.Date;
+import java.util.HashMap;
+
+import GestionSallesMachines.Salle;
 
 public class TempsDeSeance {
 	private Date jour;
 	private Date heureDeb;
 	private Date heureFin;
+	
+	private HashMap<Seance,Salle> hmSeanceSalle;
 }
