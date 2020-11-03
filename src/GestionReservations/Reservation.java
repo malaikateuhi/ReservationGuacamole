c9eb9fcb8d2b8cc5a08057f8946392c1bf76fc8f
meng7777 <66383612+meng7777@users.noreply.github.com>
2020-11-03 10:45:50 +0100

    Update Reservation.java

--- src/GestionReservations/Reservation.java
+++ src/GestionReservations/Reservation.java
@@ -1,10 +1,16 @@
 package GestionReservations;
 import java.util.Date;
 
+import GestionSallesMachines.Machine;
+import GestionUtilisateurs.Etudiant;
+
 public class Reservation {
 	private Date jour;
 	private Date heureDeb;
 	private Date heureFin;
 	private String etat;
 	//private int duree; //en heure
+	
+	private Machine machine;
+	private Etudiant etudiant;
 }
