196583a185661697a34f14076435ab2cfbe02acb
meng7777 <66383612+meng7777@users.noreply.github.com>
2020-11-03 17:07:54 +0100

    Update Reservation.java

--- src/GestionReservations/Reservation.java
+++ src/GestionReservations/Reservation.java
@@ -1,15 +1,44 @@
 package GestionReservations;
 import java.util.Date;
 
-import GestionSallesMachines.Machine;
-import GestionSallesMachines.Salle;
-import GestionUtilisateurs.Etudiant;
 
 public class Reservation {
 	private Date jour;
 	private Date heureDeb;
 	private Date heureFin;
 	private EtatReservation etat;
+	
 	//private int duree; //en heure
+	public Reservation(Date jour, Date heureDeb, Date heureFin, EtatReservation etat) {
+		super();
+		this.jour = jour;
+		this.heureDeb = heureDeb;
+		this.heureFin = heureFin;
+		this.etat = etat;
+	}
+	public Date getJour() {
+		return jour;
+	}
+	public void setJour(Date jour) {
+		this.jour = jour;
+	}
+	public Date getHeureDeb() {
+		return heureDeb;
+	}
+	public void setHeureDeb(Date heureDeb) {
+		this.heureDeb = heureDeb;
+	}
+	public Date getHeureFin() {
+		return heureFin;
+	}
+	public void setHeureFin(Date heureFin) {
+		this.heureFin = heureFin;
+	}
+	public EtatReservation getEtat() {
+		return etat;
+	}
+	public void setEtat(EtatReservation etat) {
+		this.etat = etat;
+	}
 
 }
