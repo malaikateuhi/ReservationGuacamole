f0cb680523f6ea7d62b15d5e98852b0ef0fbb534
meng7777 <66383612+meng7777@users.noreply.github.com>
2020-11-03 16:46:00 +0100

    Update Utilisateur.java

--- src/GestionUtilisateurs/Utilisateur.java
+++ src/GestionUtilisateurs/Utilisateur.java
@@ -6,4 +6,32 @@ public abstract class Utilisateur {
 	private String nom;
 	private String prenom;
 	
+	public String getIdentifiant() {
+		return identifiant;
+	}
+	public void setIdentifiant(String identifiant) {
+		this.identifiant = identifiant;
+	}
+	public String getMdp() {
+		return mdp;
+	}
+	public void setMdp(String mdp) {
+		this.mdp = mdp;
+	}
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public String getPrenom() {
+		return prenom;
+	}
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+	public Utilisateur() {
+		
+	}
+	
 }
