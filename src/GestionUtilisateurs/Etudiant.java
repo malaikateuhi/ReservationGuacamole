33c4128590787d01ee9ddb20162c0c3df49e836c
malaikateuhi <malaikateuhi@gmail.com>
2020-11-03 14:02:17 +0100

    Constructeur Etudiant

--- src/GestionUtilisateurs/Etudiant.java
+++ src/GestionUtilisateurs/Etudiant.java
@@ -1,9 +1,16 @@
 package GestionUtilisateurs;
 
 public class Etudiant extends Utilisateur {
+	
+	
+	public Etudiant(String identifiant, String mdp, String nom, String prenom) {
+		super(identifiant, mdp, nom, prenom);
+	}
+	
 	public void reserver() {
 		
 	}
+	
 	public void annulerReservation() {
 		
 	}
