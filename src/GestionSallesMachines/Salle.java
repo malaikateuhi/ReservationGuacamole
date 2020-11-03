a793aa45a8aabedfecb59067ac87f550a00c8cbf
meng7777 <66383612+meng7777@users.noreply.github.com>
2020-11-03 17:08:03 +0100

    Update Salle.java

--- src/GestionSallesMachines/Salle.java
+++ src/GestionSallesMachines/Salle.java
@@ -12,4 +12,33 @@ public class Salle {
 	private ArrayList<Machine> lstMachines;
 	//private HashMap<TempsDeSeance,Seance> hmSeance;
 	
+	public Salle() {
+		
+	}
+	public Salle(String nomSalle, EtatMachineSalle etat, ArrayList<Machine> lstMachines) {
+		super();
+		this.nomSalle = nomSalle;
+		this.etat = etat;
+		this.lstMachines = lstMachines;
+	}
+	public String getNomSalle() {
+		return nomSalle;
+	}
+	
+	public void setNomSalle(String nomSalle) {
+		this.nomSalle = nomSalle;
+	}
+	public EtatMachineSalle getEtat() {
+		return etat;
+	}
+	public void setEtat(EtatMachineSalle etat) {
+		this.etat = etat;
+	}
+	public ArrayList<Machine> getLstMachines() {
+		return lstMachines;
+	}
+	public void setLstMachines(ArrayList<Machine> lstMachines) {
+		this.lstMachines = lstMachines;
+	}
+	
 }
