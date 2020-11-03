15b1a456182aab9c4e7e5e1b3f46eeb9d1c4f7f6
meng7777 <66383612+meng7777@users.noreply.github.com>
2020-11-03 17:07:58 +0100

    Update TempsDeSeance.java

--- src/GestionReservations/TempsDeSeance.java
+++ src/GestionReservations/TempsDeSeance.java
@@ -9,5 +9,48 @@ public class TempsDeSeance {
 	private Date heureDeb;
 	private Date heureFin;
 	
+	public TempsDeSeance() {
+		
+	}
+	public TempsDeSeance(Date jour, Date heureDeb, Date heureFin, HashMap<Seance, Salle> hmSeanceSalle) {
+		super();
+		this.jour = jour;
+		this.heureDeb = heureDeb;
+		this.heureFin = heureFin;
+		this.hmSeanceSalle = hmSeanceSalle;
+	}
+
+	public Date getJour() {
+		return jour;
+	}
+
+	public void setJour(Date jour) {
+		this.jour = jour;
+	}
+
+	public Date getHeureDeb() {
+		return heureDeb;
+	}
+
+	public void setHeureDeb(Date heureDeb) {
+		this.heureDeb = heureDeb;
+	}
+
+	public Date getHeureFin() {
+		return heureFin;
+	}
+
+	public void setHeureFin(Date heureFin) {
+		this.heureFin = heureFin;
+	}
+
+	public HashMap<Seance, Salle> getHmSeanceSalle() {
+		return hmSeanceSalle;
+	}
+
+	public void setHmSeanceSalle(HashMap<Seance, Salle> hmSeanceSalle) {
+		this.hmSeanceSalle = hmSeanceSalle;
+	}
+
 	private HashMap<Seance,Salle> hmSeanceSalle;
 }
