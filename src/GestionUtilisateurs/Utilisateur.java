3e4445ea4487d1148ed639140fb6dd707b975513
malaikateuhi <malaikateuhi@gmail.com>
2020-11-03 12:33:26 +0100

    Ajout constructeur et getters pour Utilisateur

--- src/GestionUtilisateurs/Utilisateur.java
+++ src/GestionUtilisateurs/Utilisateur.java
@@ -6,4 +6,25 @@ public abstract class Utilisateur {
 	private String nom;
 	private String prenom;
 	
+	public Utilisateur(String identifiant, String mdp, String nom, String prenom) {
+		super();
+		this.identifiant = identifiant;
+		this.mdp = mdp;
+		this.nom = nom;
+		this.prenom = prenom;
+	}
+	
+	protected String getIdentifiant() {
+		return identifiant;
+	}
+	protected String getMdp() {
+		return mdp;
+	}
+	protected String getNom() {
+		return nom;
+	}
+	protected String getPrenom() {
+		return prenom;
+	}
+	
 }
