bdc7ae25aad1a1c97c5813d51df3c6b9702fb3c0
meng7777 <66383612+meng7777@users.noreply.github.com>
2020-11-03 17:08:08 +0100

    Update Groupe.java

--- src/GestionUtilisateurs/Groupe.java
+++ src/GestionUtilisateurs/Groupe.java
@@ -7,4 +7,30 @@ public class Groupe {
 	private String nomGroupe;
 	
 	private ArrayList<Etudiant> lstEtu;
+
+	public String getNumGroupe() {
+		return numGroupe;
+	}
+
+	public void setNumGroupe(String numGroupe) {
+		this.numGroupe = numGroupe;
+	}
+
+	public String getNomGroupe() {
+		return nomGroupe;
+	}
+
+	public void setNomGroupe(String nomGroupe) {
+		this.nomGroupe = nomGroupe;
+	}
+
+	public ArrayList<Etudiant> getLstEtu() {
+		return lstEtu;
+	}
+
+	public void setLstEtu(ArrayList<Etudiant> lstEtu) {
+		this.lstEtu = lstEtu;
+	}
+	
+	
 }
